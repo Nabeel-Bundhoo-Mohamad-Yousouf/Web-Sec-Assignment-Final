1bbcfc926b37eabee82894682e78c764da05a265
urshita22 <urshita.canhye@umail.uom.ac.mu>
2026-05-02 20:17:14 +0400

    Add user rentals route to API

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,10 @@ use Illuminate\Support\Facades\Route;
 // Public API: Anyone can see the book list
 Route::get('/books', [BookApiController::class, 'index']);
 Route::get('/books/{id}', [BookApiController::class, 'show']);
+Route::get('/reviews', [BookApiController::class, 'indexReviews']);
 
 // Protected API: Only "Urshita" with a Token can post reviews
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/books/{id}/reviews', [BookApiController::class, 'storeReview']);
+    Route::get('/users/{id}/rentals', [BookApiController::class, 'userRentals']);
 });
